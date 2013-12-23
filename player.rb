7ffa718844ab2f5456df9c679614cd3a98205e14
mpataki <matpataki@gmail.com>
2013-12-22 19:33:21 -0500

    level 3

--- player.rb
+++ player.rb
@@ -1,9 +1,22 @@
 class Player
+
+  def defend?(warrior)
+    return false if warrior.health > 3
+    unless warrior.feel.enemy?
+      warrior.rest!
+    else
+      warrior.walk!(:backward)
+    end
+    return true
+  end
+
   def play_turn(warrior)
+    return if defend?(warrior)
     if warrior.feel.enemy?
       warrior.attack!
     else
       warrior.walk!
     end
   end
+
 end
